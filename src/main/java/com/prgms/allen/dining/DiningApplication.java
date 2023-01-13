2b2b35daabda13dc4b89ee05dc50d103bbede7e2
Seonho Kim <sunhowold@naver.com>
2023-01-13 16:31:57 +0900

    initial commit

---
+++ src/main/java/com/prgms/allen/dining/DiningApplication.java
@@ -0,0 +1,13 @@
+package com.prgms.allen.dining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiningApplication.class, args);
+	}
+
+}
