234cc1d79ab06677fd92fab83c0c171e23e8e8a6
dlxortmd987 <dlxortmd987@gmail.com>
2023-01-15 23:11:41 +0900

    Feat(API):  고객 회원가입 구현

--- src/main/java/com/prgms/allen/dining/web/customer/CustomerApi.java
+++ src/main/java/com/prgms/allen/dining/web/customer/CustomerApi.java
@@ -1,4 +1,28 @@
 package com.prgms.allen.dining.web.customer;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.prgms.allen.dining.domain.customer.CustomerService;
+import com.prgms.allen.dining.domain.customer.dto.CustomerSignupRequest;
+
+@RestController
+@RequestMapping("/api/customers/")
 public class CustomerApi {
+
+	private final CustomerService customerService;
+
+	public CustomerApi(CustomerService customerService) {
+		this.customerService = customerService;
+	}
+
+	@PostMapping("/signup")
+	public ResponseEntity<Void> signup(CustomerSignupRequest customerSignupRequest) {
+		customerService.signup(customerSignupRequest);
+		return ResponseEntity.status(HttpStatus.CREATED)
+			.build();
+	}
 }
