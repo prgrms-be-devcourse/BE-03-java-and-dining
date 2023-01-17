f6980870bd54179dc92e08b62730700e2b88314e
Seonho Kim <sunhowold@naver.com>
2023-01-17 16:23:42 +0900

    Refactor: 비즈니스 정책 변경에 따른 패키지 구조 변경
    ### 변경 전
    
    ex) web/customer/me...
    
    ### 변경 후
    
    ex) web/domain/customer/...

--- src/main/java/com/prgms/allen/dining/web/domain/owner/me/OwnerMyPageApi.java
+++ src/main/java/com/prgms/allen/dining/web/domain/owner/me/OwnerMyPageApi.java
@@ -1,9 +1,9 @@
-package com.prgms.allen.dining.web.customer.restaurant;
+package com.prgms.allen.dining.web.domain.owner.me;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/customer/api/restaurants")
-public class CustomerRestaurantApi {
+@RequestMapping("/owner/api/me")
+public class OwnerMyPageApi {
 }
--- src/main/java/com/prgms/allen/dining/web/domain/owner/restaurant/OwnerRestaurantApi.java
+++ src/main/java/com/prgms/allen/dining/web/domain/owner/restaurant/OwnerRestaurantApi.java
@@ -1,9 +1,9 @@
-package com.prgms.allen.dining.web.customer.reservation;
+package com.prgms.allen.dining.web.domain.owner.restaurant;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/customer/api/reservations")
-public class CustomerReservationApi {
+@RequestMapping("/owner/api/restaurants")
+public class OwnerRestaurantApi {
 }
