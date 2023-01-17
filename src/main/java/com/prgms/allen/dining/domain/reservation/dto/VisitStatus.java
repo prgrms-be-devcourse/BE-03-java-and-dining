3eb6bcdba06bcc4d745f793d1a1eebfbf40ce993
dlxortmd987 <dlxortmd987@gmail.com>
2023-01-17 22:16:13 +0900

    Feat: 고객의 방문 상태를 나타내는 vo 생성

---
+++ src/main/java/com/prgms/allen/dining/domain/reservation/dto/VisitStatus.java
@@ -0,0 +1,22 @@
+package com.prgms.allen.dining.domain.reservation.dto;
+
+import java.util.List;
+
+import com.prgms.allen.dining.domain.reservation.entity.ReservationStatus;
+
+public enum VisitStatus {
+
+	PLANNED(List.of(ReservationStatus.PENDING, ReservationStatus.CONFIRMED)),
+	DONE(List.of(ReservationStatus.VISITED)),
+	CANCEL(List.of(ReservationStatus.CANCELLED, ReservationStatus.NO_SHOW));
+
+	private final List<ReservationStatus> statuses;
+
+	VisitStatus(List<ReservationStatus> statuses) {
+		this.statuses = statuses;
+	}
+
+	public List<ReservationStatus> getStatuses() {
+		return statuses;
+	}
+}
