bcfcefa8573eb563097e991e1119461222283d32
Surin Lee <leesurin99@gmail.com>
2023-01-17 17:54:36 +0900

    Feat(exception): 점주가 또 식당을 등록할 경우에 대한 예외 추가

--- src/main/java/com/prgms/allen/dining/global/error/ErrorCode.java
+++ src/main/java/com/prgms/allen/dining/global/error/ErrorCode.java
@@ -5,7 +5,8 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
 
 	INVALID_PARAMETER(HttpStatus.BAD_REQUEST, "파라미터 값을 확인해주세요."),
-	INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 에러입니다.");
+	INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 에러입니다."),
+	DUPLICATE_ERROR(HttpStatus.BAD_REQUEST, "중복 생성 할 수 없습니다.");
 
 	private final HttpStatus status;
 	private final String message;
