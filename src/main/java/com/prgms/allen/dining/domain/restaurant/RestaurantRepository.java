2787eade20ffbab3d945f48c5aa7dd3b61019574
yewon.seo <dodnjs1241@naver.com>
2023-01-19 01:52:36 +0900

    Feat(Repository) : 레스토랑 목록 페이징 조회를 위한 쿼리 메서드 추가

--- src/main/java/com/prgms/allen/dining/domain/restaurant/RestaurantRepository.java
+++ src/main/java/com/prgms/allen/dining/domain/restaurant/RestaurantRepository.java
@@ -1,5 +1,7 @@
 package com.prgms.allen.dining.domain.restaurant;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.prgms.allen.dining.domain.restaurant.entity.Restaurant;
@@ -7,4 +9,6 @@ import com.prgms.allen.dining.domain.restaurant.entity.Restaurant;
 public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
 
 	boolean existsRestaurantByOwnerId(Long ownerId);
+
+	Page<Restaurant> findAll(Pageable pageable);
 }
