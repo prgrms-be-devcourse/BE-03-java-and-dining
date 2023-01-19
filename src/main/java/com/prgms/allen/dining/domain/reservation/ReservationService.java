e78e4cc96fef097c4c4d33a33dfe08fb19f5afa4
Surin Lee <leesurin99@gmail.com>
2023-01-19 18:48:52 +0900

    Feat(service): 날짜, 시간, 인원수를 전달하면 예약 가능한 여부를 조회하는 기능 구현

--- src/main/java/com/prgms/allen/dining/domain/reservation/ReservationService.java
+++ src/main/java/com/prgms/allen/dining/domain/reservation/ReservationService.java
@@ -1,5 +1,9 @@
 package com.prgms.allen.dining.domain.reservation;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
@@ -9,10 +13,14 @@ import org.springframework.transaction.annotation.Transactional;
 import com.prgms.allen.dining.domain.reservation.dto.ReservationSimpleResponse;
 import com.prgms.allen.dining.domain.reservation.entity.ReservationStatus;
 import com.prgms.allen.dining.domain.restaurant.RestaurantService;
+import com.prgms.allen.dining.domain.restaurant.entity.Restaurant;
 
 @Service
 @Transactional(readOnly = true)
 public class ReservationService {
+
+	private static final List<ReservationStatus> TAKEN_STATUS_LIST =
+		List.of(ReservationStatus.CONFIRMED, ReservationStatus.PENDING);
 
 	private final ReservationRepository reservationRepository;
 	private final RestaurantService restaurantService;
@@ -35,5 +43,19 @@ public class ReservationService {
 				.map(ReservationSimpleResponse::new)
 				.toList()
 		);
+	}
+
+	public boolean isAvailableReserve(
+		Restaurant restaurant,
+		LocalDateTime requestTime,
+		int numberOfPeople
+	) {
+
+		Optional<Integer> totalCount = reservationRepository.countTotalVisitorCount(restaurant,
+			requestTime.toLocalDate(),
+			requestTime.toLocalTime(),
+			TAKEN_STATUS_LIST);
+
+		return restaurant.isAvailable(totalCount.get(), numberOfPeople);
 	}
 }
