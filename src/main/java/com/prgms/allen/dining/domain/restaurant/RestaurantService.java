55566341020aae92830d4e3ae6fe9e3960d61f81
Surin Lee <leesurin99@gmail.com>
2023-01-19 19:04:14 +0900

    Feat(service): 하나의 식당 정보를 가져오는 기능 구현

--- src/main/java/com/prgms/allen/dining/domain/restaurant/RestaurantService.java
+++ src/main/java/com/prgms/allen/dining/domain/restaurant/RestaurantService.java
@@ -1,13 +1,20 @@
 package com.prgms.allen.dining.domain.restaurant;
 
 import java.text.MessageFormat;
+import java.util.List;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.prgms.allen.dining.domain.member.MemberService;
 import com.prgms.allen.dining.domain.member.entity.Member;
+import com.prgms.allen.dining.domain.restaurant.dto.ClosingDayRes;
+import com.prgms.allen.dining.domain.restaurant.dto.MenuSimpleRes;
 import com.prgms.allen.dining.domain.restaurant.dto.RestaurantCreateReq;
+import com.prgms.allen.dining.domain.restaurant.dto.RestaurantDetailResForCustomer;
+import com.prgms.allen.dining.domain.restaurant.dto.RestaurantDetailResForOwner;
+import com.prgms.allen.dining.domain.restaurant.entity.ClosingDay;
+import com.prgms.allen.dining.domain.restaurant.entity.Menu;
 import com.prgms.allen.dining.domain.restaurant.entity.Restaurant;
 import com.prgms.allen.dining.global.error.ErrorCode;
 import com.prgms.allen.dining.global.error.exception.NotFoundResourceException;
@@ -47,9 +54,54 @@ public class RestaurantService {
 		return restaurant.getId();
 	}
 
+	public Restaurant findRestaurantById(Long restaurantId) {
+		return restaurantRepository.findById(restaurantId)
+			.orElseThrow(() -> {
+				throw new NotFoundResourceException(
+					ErrorCode.NOT_FOUND_RESOURCE,
+					MessageFormat.format("Cannot find Restaurant entity for restaurant id = {0}", restaurantId)
+				);
+			});
+	}
+
+	public RestaurantDetailResForCustomer getRestaurant(Long restaurantId) {
+		final Restaurant restaurant = findRestaurantById(restaurantId);
+
+		return new RestaurantDetailResForCustomer(restaurant,
+			toMenuSimpleResList(restaurant.getMinorMenu()),
+			toClosingDayResList(restaurant.getClosingDays())
+		);
+	}
+
+	public RestaurantDetailResForOwner getRestaurant(Long restaurantId, Long ownerId) {
+		Member owner = memberService.findOwnerById(ownerId);
+
+		Restaurant restaurant = restaurantRepository.findByIdAndOwner(restaurantId, owner)
+			.orElseThrow(() -> {
+				throw new IllegalArgumentException("No Authorization");
+			});
+
+		return new RestaurantDetailResForOwner(restaurant,
+			toMenuSimpleResList(restaurant.getMinorMenu()),
+			toClosingDayResList(restaurant.getClosingDays())
+		);
+	}
+
 	private void validAlreadyHasRestaurant(Long ownerId) {
 		if (restaurantRepository.existsRestaurantByOwnerId(ownerId)) {
 			throw new RestaurantDuplicateCreationException(ErrorCode.DUPLICATE_ERROR);
 		}
+	}
+
+	private List<MenuSimpleRes> toMenuSimpleResList(List<Menu> menu) {
+		return menu.stream()
+			.map(MenuSimpleRes::new)
+			.toList();
+	}
+
+	private List<ClosingDayRes> toClosingDayResList(List<ClosingDay> closingDayList) {
+		return closingDayList.stream()
+			.map(ClosingDayRes::new)
+			.toList();
 	}
 }
--- src/test/java/com/prgms/allen/dining/domain/restaurant/RestaurantServiceTest.java
+++ src/test/java/com/prgms/allen/dining/domain/restaurant/RestaurantServiceTest.java
@@ -19,7 +19,10 @@ import com.prgms.allen.dining.domain.member.entity.MemberType;
 import com.prgms.allen.dining.domain.restaurant.dto.ClosingDayCreateReq;
 import com.prgms.allen.dining.domain.restaurant.dto.MenuCreateReq;
 import com.prgms.allen.dining.domain.restaurant.dto.RestaurantCreateReq;
+import com.prgms.allen.dining.domain.restaurant.entity.ClosingDay;
 import com.prgms.allen.dining.domain.restaurant.entity.FoodType;
+import com.prgms.allen.dining.domain.restaurant.entity.Menu;
+import com.prgms.allen.dining.domain.restaurant.entity.Restaurant;
 import com.prgms.allen.dining.global.error.exception.RestaurantDuplicateCreationException;
 
 class RestaurantServiceTest {
@@ -28,14 +31,10 @@ class RestaurantServiceTest {
 	private final MemberRepository memberRepository = new FakeMemberRepository();
 	private final MemberService memberService = new MemberService(memberRepository);
 	private final RestaurantService restaurantService = new RestaurantService(
-		restaurantRepository,
-		memberService
+		restaurantRepository, memberService
 	);
 
 	private Member savedOwner;
-	private List<ClosingDayCreateReq> closingDayList;
-	private List<MenuCreateReq> menuList;
-	private RestaurantCreateReq restaurantCreateReq;
 
 	@BeforeEach
 	void setUp() {
@@ -46,27 +45,13 @@ class RestaurantServiceTest {
 			"qwer1234!",
 			MemberType.OWNER);
 		savedOwner = memberRepository.save(owner);
-
-		closingDayList = List.of(new ClosingDayCreateReq(DayOfWeek.MONDAY));
-		menuList = List.of(new MenuCreateReq("맛있는 밥", BigInteger.valueOf(10000), "맛있어용"));
-
-		restaurantCreateReq = new RestaurantCreateReq(
-			FoodType.KOREAN,
-			"유명 레스토랑",
-			30,
-			LocalTime.of(11, 0),
-			LocalTime.of(21, 0),
-			"서울특별시 강남구 어딘가로 123 무슨빌딩 1층",
-			"우리는 유명한 한식당입니다.",
-			"0211112222",
-			menuList,
-			closingDayList);
 	}
 
 	@Test
 	@DisplayName("점주는 식당을 등록할 수 있다.")
 	public void testSave() {
-		System.out.println(savedOwner.getId());
+		// given
+		RestaurantCreateReq restaurantCreateReq = createRestaurantCreateReq();
 
 		// when
 		restaurantService.save(restaurantCreateReq, savedOwner.getId());
@@ -78,11 +63,58 @@ class RestaurantServiceTest {
 	@Test
 	@DisplayName("점주가 식당을 2개이상 생성하려할 경우 RestaurantDuplicateCreationException 을 던진다.")
 	public void failSave() {
-		System.out.println(savedOwner.getId());
+		// given
+		RestaurantCreateReq restaurantCreateReq = createRestaurantCreateReq();
+		RestaurantCreateReq restaurantCreateReq2 = createRestaurantCreateReq();
 
-		final RestaurantCreateReq restaurantCreateReq2 = new RestaurantCreateReq(
+		restaurantService.save(restaurantCreateReq, savedOwner.getId());
+
+		assertThatThrownBy(() -> restaurantService.save(restaurantCreateReq2, savedOwner.getId()))
+			.isInstanceOf(RestaurantDuplicateCreationException.class);
+	}
+
+	@Test
+	@DisplayName("식당의 상세정보를 조회할 수 있다.")
+	public void testGetOneRestaurant() {
+		// given
+		Restaurant restaurant = createRestaurant();
+
+		// when
+		Restaurant findRestaurant = restaurantService.findRestaurantById(restaurant.getId());
+
+		// then
+		assertThat(restaurant)
+			.usingRecursiveComparison()
+			.isEqualTo(findRestaurant);
+	}
+
+	private Restaurant createRestaurant() {
+		List<ClosingDay> closingDays = List.of(new ClosingDay(DayOfWeek.MONDAY));
+		List<Menu> menu = List.of(new Menu("맛있는 밥", BigInteger.valueOf(10000), "맛있어용"));
+
+		Restaurant restaurant = new Restaurant(
+			savedOwner,
+			FoodType.WESTERN,
+			"유명한 레스토랑",
+			30,
+			LocalTime.of(12, 0),
+			LocalTime.of(20, 0),
+			"서울특별시 어딘구 어딘가로 222",
+			"BTS가 다녀간 유명한 레스토랑",
+			"023334444",
+			menu,
+			closingDays
+		);
+		return restaurantRepository.save(restaurant);
+	}
+
+	private RestaurantCreateReq createRestaurantCreateReq() {
+		List<ClosingDayCreateReq> closingDayList = List.of(new ClosingDayCreateReq(DayOfWeek.MONDAY));
+		List<MenuCreateReq> menuList = List.of(new MenuCreateReq("맛있는 밥", BigInteger.valueOf(10000), "맛있어용"));
+
+		return new RestaurantCreateReq(
 			FoodType.KOREAN,
-			"유명 레스토랑인척하는레스토랑",
+			"유명 레스토랑",
 			30,
 			LocalTime.of(11, 0),
 			LocalTime.of(21, 0),
@@ -90,12 +122,6 @@ class RestaurantServiceTest {
 			"우리는 유명한 한식당입니다.",
 			"0211112222",
 			menuList,
-			closingDayList
-		);
-
-		restaurantService.save(restaurantCreateReq, savedOwner.getId());
-
-		assertThatThrownBy(() -> restaurantService.save(restaurantCreateReq2, savedOwner.getId()))
-			.isInstanceOf(RestaurantDuplicateCreationException.class);
+			closingDayList);
 	}
 }
