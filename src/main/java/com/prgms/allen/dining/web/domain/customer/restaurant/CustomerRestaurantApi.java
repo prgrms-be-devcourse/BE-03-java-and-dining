e9d139718735bad556955d0c050e4a5957a9ea1a
yewon.seo <dodnjs1241@naver.com>
2023-01-19 18:03:08 +0900

    Feat(api) : 레스토랑 명으로 N건 조회 API 구현

--- src/main/java/com/prgms/allen/dining/web/domain/customer/restaurant/CustomerRestaurantApi.java
+++ src/main/java/com/prgms/allen/dining/web/domain/customer/restaurant/CustomerRestaurantApi.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.prgms.allen.dining.domain.restaurant.RestaurantService;
@@ -24,6 +25,15 @@ public class CustomerRestaurantApi {
 	public ResponseEntity<Page<RestaurantSimpleRes>> getRestaurants(Pageable pageable) {
 
 		Page<RestaurantSimpleRes> restaurants = restaurantService.getRestaurantList(pageable);
+
+		return ResponseEntity.ok(restaurants);
+	}
+
+	@GetMapping("/search")
+	public ResponseEntity<Page<RestaurantSimpleRes>> getRestaurantsContaining(Pageable pageable,
+		@RequestParam String restaurantName) {
+
+		Page<RestaurantSimpleRes> restaurants = restaurantService.getRestaurantsContains(pageable, restaurantName);
 
 		return ResponseEntity.ok(restaurants);
 	}
