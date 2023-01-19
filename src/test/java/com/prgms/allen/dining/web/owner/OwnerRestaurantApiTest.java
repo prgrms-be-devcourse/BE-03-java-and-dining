b86a243a340c3e5f8a35d985a355d2a839a3662d
Surin Lee <leesurin99@gmail.com>
2023-01-19 19:05:35 +0900

    Feat(API): 점주가 자신의 식당 정보를 조회하는 기능 구현

--- src/test/java/com/prgms/allen/dining/web/owner/OwnerRestaurantApiTest.java
+++ src/test/java/com/prgms/allen/dining/web/owner/OwnerRestaurantApiTest.java
@@ -3,6 +3,7 @@ package com.prgms.allen.dining.web.owner;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.*;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.request.RequestDocumentation.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -10,6 +11,8 @@ import java.math.BigInteger;
 import java.time.DayOfWeek;
 import java.time.LocalTime;
 import java.util.List;
+
+import javax.transaction.Transactional;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -20,7 +23,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
@@ -28,10 +30,14 @@ import com.prgms.allen.dining.domain.member.MemberRepository;
 import com.prgms.allen.dining.domain.member.dto.MemberSignupRequest;
 import com.prgms.allen.dining.domain.member.entity.Member;
 import com.prgms.allen.dining.domain.member.entity.MemberType;
+import com.prgms.allen.dining.domain.restaurant.RestaurantRepository;
 import com.prgms.allen.dining.domain.restaurant.dto.ClosingDayCreateReq;
 import com.prgms.allen.dining.domain.restaurant.dto.MenuCreateReq;
 import com.prgms.allen.dining.domain.restaurant.dto.RestaurantCreateReq;
+import com.prgms.allen.dining.domain.restaurant.entity.ClosingDay;
 import com.prgms.allen.dining.domain.restaurant.entity.FoodType;
+import com.prgms.allen.dining.domain.restaurant.entity.Menu;
+import com.prgms.allen.dining.domain.restaurant.entity.Restaurant;
 
 @AutoConfigureRestDocs
 @AutoConfigureMockMvc
@@ -45,14 +51,17 @@ class OwnerRestaurantApiTest {
 	@Autowired
 	private MemberRepository memberRepository;
 
+	@Autowired
+	private RestaurantRepository restaurantRepository;
+
 	private final ObjectMapper objectMapper = new ObjectMapper();
-	private static Member owner;
 
 	@Test
 	@DisplayName("점주는 식당을 하나 등록할 수 있다.")
 	void testSave() throws Exception {
 
-		owner = createOwner();
+		Member owner = createOwner();
+
 		mockMvc.perform(post("/owner/api/restaurants?ownerId=" + owner.getId())
 				.contentType(MediaType.APPLICATION_JSON)
 				.content(objectMapper.registerModule(new JavaTimeModule())
@@ -81,6 +90,40 @@ class OwnerRestaurantApiTest {
 			);
 	}
 
+	@Test
+	@DisplayName("점주는 자신의 식당을 조회할 수 있다.")
+	void testGetMyRestaurant() throws Exception {
+
+		Member owner = createOwner();
+		Restaurant restaurant = createRestaurant(owner);
+
+		mockMvc.perform(
+				get("/owner/api/restaurants/{restaurantId}?ownerId=" + owner.getId(), restaurant.getId()))
+			.andExpect(status().isOk())
+			.andDo(print())
+			.andDo(document("owner-get-owned-restaurant",
+				requestParameters(
+					parameterWithName("ownerId").description("It must same as owner id of restaurant")
+				),
+				responseFields(
+					fieldWithPath("foodType").type(JsonFieldType.STRING).description("food type"),
+					fieldWithPath("name").type(JsonFieldType.STRING).description("restaurant name"),
+					fieldWithPath("capacity").type(JsonFieldType.NUMBER).description("restaurant capacity"),
+					fieldWithPath("openTime").type(JsonFieldType.STRING).description("restaurant open time"),
+					fieldWithPath("lastOrderTime").type(JsonFieldType.STRING).description("restaurant last order time"),
+					fieldWithPath("location").type(JsonFieldType.STRING).description("restaurant location"),
+					fieldWithPath("description").type(JsonFieldType.STRING).description("restaurant description"),
+					fieldWithPath("phone").type(JsonFieldType.STRING).description("restaurant phone"),
+					fieldWithPath("menuList").type(JsonFieldType.ARRAY).optional().description("menu list"),
+					fieldWithPath("menuList[].name").type(JsonFieldType.STRING).optional().description("menu name"),
+					fieldWithPath("menuList[].price").type(JsonFieldType.NUMBER).optional().description("menu price"),
+					fieldWithPath("closingDays").type(JsonFieldType.ARRAY).optional().description("closing days"),
+					fieldWithPath("closingDays[].dayOfWeek").optional().type(JsonFieldType.STRING)
+						.description("closing day of week")
+				))
+			);
+	}
+
 	private Member createOwner() {
 		String nickName = "이세상에제일가는짱구";
 		MemberSignupRequest memberSignupRequest =
@@ -91,12 +134,7 @@ class OwnerRestaurantApiTest {
 				"1q2w3e4r!",
 				MemberType.OWNER);
 
-		memberRepository.save(memberSignupRequest.toEntity());
-		return memberRepository.findAll()
-			.stream()
-			.filter(member -> nickName.equals(member.getNickname()))
-			.findAny()
-			.get();
+		return memberRepository.save(memberSignupRequest.toEntity());
 	}
 
 	private RestaurantCreateReq restaurantCreateResource() {
@@ -118,5 +156,25 @@ class OwnerRestaurantApiTest {
 			"0211112222",
 			menuList,
 			closingDayList);
+	}
+
+	private Restaurant createRestaurant(Member owner) {
+		List<ClosingDay> closingDays = List.of(new ClosingDay(DayOfWeek.MONDAY));
+		List<Menu> menu = List.of(new Menu("맛있는 밥", BigInteger.valueOf(10000), "맛있어용"));
+
+		Restaurant restaurant = new Restaurant(
+			owner,
+			FoodType.WESTERN,
+			"유명한 레스토랑",
+			30,
+			LocalTime.of(12, 0),
+			LocalTime.of(20, 0),
+			"서울특별시 어딘구 어딘가로 222",
+			"BTS가 다녀간 유명한 레스토랑",
+			"023334444",
+			menu,
+			closingDays
+		);
+		return restaurantRepository.save(restaurant);
 	}
 }
