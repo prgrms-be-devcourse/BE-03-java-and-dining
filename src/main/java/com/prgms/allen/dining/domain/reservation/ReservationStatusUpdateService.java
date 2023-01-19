8b1c7a89a865a66e66a5c8b2572631446b4922e7
Seonho Kim <sunhowold@naver.com>
2023-01-19 21:08:39 +0900

    Refactor: 예약 상태 수정 서비스 객체 로깅 추가, Transactional 추가

--- src/main/java/com/prgms/allen/dining/domain/reservation/ReservationStatusUpdateService.java
+++ src/main/java/com/prgms/allen/dining/domain/reservation/ReservationStatusUpdateService.java
@@ -1,11 +1,16 @@
 package com.prgms.allen.dining.domain.reservation;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.prgms.allen.dining.domain.reservation.entity.Reservation;
 
 @Service
 public class ReservationStatusUpdateService {
+
+	private static final Logger log = LoggerFactory.getLogger(ReservationStatusUpdateService.class);
 
 	private final ReservationService reservationService;
 
@@ -13,8 +18,10 @@ public class ReservationStatusUpdateService {
 		this.reservationService = reservationService;
 	}
 
+	@Transactional
 	public void confirmReservation(Long reservationId, Long ownerId) {
 		Reservation findReservation = reservationService.findById(reservationId);
 		findReservation.confirm(ownerId);
+		log.info("Reservation {}'s status updated to {}", reservationId, findReservation.getStatus());
 	}
 }
