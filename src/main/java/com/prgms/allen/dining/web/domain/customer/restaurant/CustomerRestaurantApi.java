4c50b433a9bdec439e8f32d2f4136c49da2e5d98
Surin Lee <leesurin99@gmail.com>
2023-01-25 16:59:14 +0900

    Feat(API): 예약 가능 날짜 리스트를 가져오는 기능 구현

--- src/main/java/com/prgms/allen/dining/web/domain/customer/restaurant/CustomerRestaurantApi.java
+++ src/main/java/com/prgms/allen/dining/web/domain/customer/restaurant/CustomerRestaurantApi.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.prgms.allen.dining.domain.restaurant.RestaurantService;
+import com.prgms.allen.dining.domain.restaurant.dto.RestaurantAvailableDatesRes;
 import com.prgms.allen.dining.domain.restaurant.dto.RestaurantDetailResForCustomer;
 import com.prgms.allen.dining.domain.restaurant.dto.RestaurantSimpleRes;
 
@@ -34,6 +35,14 @@ public class CustomerRestaurantApi {
 		Page<RestaurantSimpleRes> restaurants = restaurantService.getRestaurantList(pageable);
 
 		return ResponseEntity.ok(restaurants);
+	}
+
+	@GetMapping("/{restaurantId}/available-dates")
+	public ResponseEntity<RestaurantAvailableDatesRes> getAvailableDates(@PathVariable Long restaurantId) {
+		RestaurantAvailableDatesRes restaurantAvailableDatesRes = restaurantService.getAvailableReserveDates(
+			restaurantId);
+
+		return ResponseEntity.ok(restaurantAvailableDatesRes);
 	}
 
 }
