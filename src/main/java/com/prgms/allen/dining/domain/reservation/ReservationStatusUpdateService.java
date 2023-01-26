2b8637b365496f11b1303c737c4891358e34bc83
Seonho Kim <sunhowold@naver.com>
2023-01-26 15:11:53 +0900

    Refactor(API, service): 예약 상태 변경 시 분기 로직에 대한 책임을 서비스 객체가 갖도록 수정

--- src/main/java/com/prgms/allen/dining/domain/reservation/ReservationStatusUpdateService.java
+++ src/main/java/com/prgms/allen/dining/domain/reservation/ReservationStatusUpdateService.java
@@ -1,11 +1,14 @@
 package com.prgms.allen.dining.domain.reservation;
 
+import java.text.MessageFormat;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.prgms.allen.dining.domain.reservation.entity.Reservation;
+import com.prgms.allen.dining.web.domain.owner.reservation.ReservationStatusUpdateReq;
 
 @Service
 @Transactional
@@ -19,25 +22,38 @@ public class ReservationStatusUpdateService {
 		this.reservationService = reservationService;
 	}
 
-	public void confirm(Long reservationId, Long ownerId) {
+	public void update(Long reservationId, Long ownerId, ReservationStatusUpdateReq updateReq) {
+		switch (updateReq.status()) {
+			case "confirm" -> confirm(reservationId, ownerId);
+			case "cancel" -> cancel(reservationId, ownerId);
+			case "visit" -> visit(reservationId, ownerId);
+			case "no-show" -> noShow(reservationId, ownerId);
+			default -> throw new IllegalArgumentException(MessageFormat.format(
+				"Cannot update reservation status for status={0}. Check your Payload.",
+				updateReq.status()
+			));
+		}
+	}
+
+	private void confirm(Long reservationId, Long ownerId) {
 		Reservation findReservation = reservationService.findById(reservationId);
 		findReservation.confirm(ownerId);
 		log.info("Reservation {}'s status updated to {}", reservationId, findReservation.getStatus());
 	}
 
-	public void cancel(Long reservationId, Long ownerId) {
+	private void cancel(Long reservationId, Long ownerId) {
 		Reservation findReservation = reservationService.findById(reservationId);
 		findReservation.cancel(ownerId);
 		log.info("Reservation {}'s status updated to {}", reservationId, findReservation.getStatus());
 	}
 
-	public void visit(Long reservationId, Long ownerId) {
+	private void visit(Long reservationId, Long ownerId) {
 		Reservation findReservation = reservationService.findById(reservationId);
 		findReservation.visit(ownerId);
 		log.info("Reservation {}'s status updated to {}", reservationId, findReservation.getStatus());
 	}
 
-	public void noShow(Long reservationId, Long ownerId) {
+	private void noShow(Long reservationId, Long ownerId) {
 		Reservation findReservation = reservationService.findById(reservationId);
 		findReservation.noShow(ownerId);
 		log.info("Reservation {}'s status updated to {}", reservationId, findReservation.getStatus());
--- src/main/java/com/prgms/allen/dining/web/domain/owner/reservation/OwnerReservationApi.java
+++ src/main/java/com/prgms/allen/dining/web/domain/owner/reservation/OwnerReservationApi.java
@@ -1,7 +1,5 @@
 package com.prgms.allen.dining.web.domain.owner.reservation;
 
-import java.text.MessageFormat;
-
 import javax.validation.Valid;
 
 import org.springframework.data.domain.Page;
@@ -51,19 +49,10 @@ public class OwnerReservationApi {
 	@PatchMapping("/{reservationId}")
 	public ResponseEntity<Void> updateStatus(
 		@PathVariable Long reservationId,
-		@Valid @RequestBody ReservationStatusUpdateReq statusUpdateReq,
-		@RequestParam Long ownerId
+		@RequestParam Long ownerId,
+		@Valid @RequestBody ReservationStatusUpdateReq statusUpdateReq
 	) {
-		switch (statusUpdateReq.status()) {
-			case "confirm" -> reservationStatusUpdateService.confirm(reservationId, ownerId);
-			case "cancel" -> reservationStatusUpdateService.cancel(reservationId, ownerId);
-			case "visit" -> reservationStatusUpdateService.visit(reservationId, ownerId);
-			case "no-show" -> reservationStatusUpdateService.noShow(reservationId, ownerId);
-			default -> throw new IllegalArgumentException(MessageFormat.format(
-				"Cannot update reservation status for status={0}. Check your Payload.",
-				statusUpdateReq.status()
-			));
-		}
+		reservationStatusUpdateService.update(reservationId, ownerId, statusUpdateReq);
 		return ResponseEntity.ok()
 			.build();
 	}
