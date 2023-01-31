c5b80a8a6b19e1a94c07eab6a5f00605d03b9b72
dlxortmd987 <dlxortmd987@gmail.com>
2023-01-31 20:37:22 +0900

    Refactor: 사용하지 않는 파일 삭제

--- src/main/java/com/prgms/allen/dining/event/NotificationService.java
+++
@@ -1,9 +0,0 @@
-package com.prgms.allen.dining.event;
-
-import com.prgms.allen.dining.domain.reservation.dto.ReservationCreateReq;
-
-public interface NotificationService {
-
-	Long reserve(Long customerId, ReservationCreateReq createReq);
-
-}
